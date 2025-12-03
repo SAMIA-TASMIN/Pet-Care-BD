f22bf44e3ea8635a0237bce8d7c0099b448a6b67
SAMIA-TASMIN <novaira489@gmail.com>
2025-12-03 19:49:43 +0600

    json added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqp3V_PMAdLWs-0vGvwFYUL7ciH-qKaLY",
+  authDomain: "pet-care-bd-24d2a.firebaseapp.com",
+  projectId: "pet-care-bd-24d2a",
+  storageBucket: "pet-care-bd-24d2a.firebasestorage.app",
+  messagingSenderId: "495083686934",
+  appId: "1:495083686934:web:4118e172169d78e2af7937"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
